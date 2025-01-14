1e2d9c46f704fd9a7c8b8a71ca555b1b83eef2ec
esteban-genty <esteban.genty@laplateforme.io>
2025-01-14 12:03:39 +0100

    Fonction pour déterminer si un nombre est premier en vérifiant 4 conditions : si inférieur à 1, si égal à 2, si pair, et si multiple impair jusqu'à la racine carrée.

---
+++ Jour02/JOB05/index.php
@@ -0,0 +1,29 @@
+<?php
+function primeNumber($ligne): bool
+{
+    if($ligne <= 1){
+        return false;
+    }
+
+    elseif($ligne == 2){
+        return true;
+    }
+
+    elseif($ligne % 2 == 0){
+        return false;
+    }
+    $squareRoot = floor(sqrt($ligne));
+    for($i = 3; $i < $squareRoot; $i+=2){
+        if($ligne % $i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+for($number = 0; $number < 1001; $number++){
+    if(primeNumber($number)){
+        echo $number . '<br>';
+    }
+}
+?>
