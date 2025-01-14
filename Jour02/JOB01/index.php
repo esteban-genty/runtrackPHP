5a4640b2e790ad0cb8e146540cae4ebd6b6348dc
esteban-genty <esteban.genty@laplateforme.io>
2025-01-14 11:59:08 +0100

    Boucle for de 0 - 1337 qui sous ligne 42 avec une condition

---
+++ Jour02/JOB01/index.php
@@ -0,0 +1,10 @@
+<?php
+for($ligne = 0; $ligne < 1338; $ligne++){
+    if($ligne == 42){
+        echo '<u><B>' . $ligne . '</u></B><br>';
+    }
+    else{
+        echo $ligne . '<br>';
+    }
+}
+?>
