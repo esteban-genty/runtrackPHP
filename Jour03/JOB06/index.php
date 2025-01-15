46be60332da35674b3d84955fc46bbf69b44a65c
esteban-genty <esteban.genty@laplateforme.io>
2025-01-15 13:42:41 +0100

    Boucle de 57 - 0 qui affiche la $str à l'envers

---
+++ Jour03/JOB06/index.php
@@ -0,0 +1,9 @@
+<?php
+$str = "Les choses que l'on Possède finissent par nous posséder.";
+
+//Boucle de 57 - 0 qui affiche la $str à l'envers
+for($ligne = 57; $ligne >= 0; $ligne--){
+    echo $str[$ligne];
+}
+
+?>
